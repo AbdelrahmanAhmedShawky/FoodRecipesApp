7e7c98ace8c9cbaadcb00ec8e5be0474aafa484d
Abdelrahman <abdelrahmanmacios@gmail.com>
2021-10-15 17:47:24 +0200

    initial commit

---
+++ FoodRecipesAppUITests/FoodRecipesAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FoodRecipesAppUITestsLaunchTests.swift
+//  FoodRecipesAppUITests
+//
+//  Created by Abdelrahman Ahmed Shawky  on 15/10/2021.
+//
+
+import XCTest
+
+class FoodRecipesAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
