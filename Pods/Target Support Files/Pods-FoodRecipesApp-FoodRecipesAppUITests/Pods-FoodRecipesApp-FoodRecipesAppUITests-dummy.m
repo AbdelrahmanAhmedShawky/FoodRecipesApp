4535d85c91db3d4bd757b5eed287d6f8613b92c0
Abdelrahman <abdelrahmanmacios@gmail.com>
2021-10-18 21:51:47 +0200

    edit in .gintignore

--- Pods/Target Support Files/Pods-FoodRecipesApp-FoodRecipesAppUITests/Pods-FoodRecipesApp-FoodRecipesAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FoodRecipesApp_FoodRecipesAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FoodRecipesApp_FoodRecipesAppUITests
-@end
