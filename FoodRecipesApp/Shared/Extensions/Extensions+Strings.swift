e9a3329ca6384430ecadf64e82336d569863244b
Abdelrahman <abdelrahmanmacios@gmail.com>
2021-10-19 20:13:54 +0200

    refactor

---
+++ FoodRecipesApp/Shared/Extensions/Extensions+Strings.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension String {
+    func validateEmail() -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let predicate = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        return predicate.evaluate(with: self)
+    }
+}
